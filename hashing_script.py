6893d849f22be88ba0e7e4077e3ada47826cca63
shubham54191 <shubham54191@users.noreply.github.com>
2026-09-07 07:18:17 -0700

    Tidy hashing_script variable names; ignore .commandcode

--- hashing_script.py
+++ hashing_script.py
@@ -1,5 +1,5 @@
 import hashlib
 social_post_data ="this is a sample social media post for hashing"
-e_data = social_post_data.encode()
-magic_print = hashlib.sha256(e_data).hexdigest()
-print("the hash is :", magic_print)
+edata = social_post_data.encode()
+magicprint = hashlib.sha256(edata).hexdigest()
+print("the hash is :", magicprint)
